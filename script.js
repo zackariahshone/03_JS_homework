26874fc8394a46d127f7afb1ee99bce978ccf285
Zack Shone <zackariahshone@gmail.com>
2020-06-21 13:25:19 -0700

    validated the input if one of the input fields is empty it will loop over until all field have a y or an n

--- script.js
+++ script.js
@@ -8,74 +8,106 @@ let lCase = "";
 let numStr = "";
 let specChar = "";
 let pLength;
+
+ let inputU = "";
+ let inputL =  "";
+ let inputN = "";
+ let inputS = "";
+
 //creat a function to gather info from the user to decide what character usage the user would like. 
-function preferences() {  
 
-const inputU = prompt("Use Upper Case: Y N").toLowerCase();
-const inputL = prompt("Use Lower Case: Y N").toLowerCase();
-const inputN = prompt("Use Numeric: Y N").toLowerCase();
-const inputS = prompt("User Special Char: Y N").toLowerCase();
-//using the input in some logic to check and use what was selected
 
-if(inputU === "y"){
-  console.log(inputU);
-  uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  console.log(uCase);
+function testDo(){
+let userInput; 
+let userInput2;
+do{
+ userInput = prompt("y or n");
+ userInput2 = prompt("Y or N");
 }
-else if (inputU === "n"){
-  uCase = "";
-} 
-if(inputL === "y"){
-  lCase = "abcdefghijklmnopqrstuvwxyz";
-  console.log(lCase);
-}else if(inputL === "n"){
-  lCase = "";
+while(userInput === '' || userInput2 === '');
 }
-if(inputN === "y"){
-numStr = "1234567890";
-console.log(numStr);
-}else if(inputN === "n"){
-  numStr = "";
-}
-if (inputS === "y") {
-  specChar = "!@#$%^&*()";
-  console.log(specChar);
-}else if(inputS === "n"){
-  specChar = "";
-}
-//proofing the answer 
-if(inputU !== "y"||inputU !== "n" && inputL !== "y"||inputL !== "n" && inputN !== "y"||inputN !== "n"&&
-inputS !== "y"||inputS !== "n"){
-  alert("not all preferences answered")
-}
+ 
 
-//create on string out of the selecting options 
-let passChar = uCase.concat(lCase,numStr,specChar);
-//checking what we are working with
-console.log("this is the new selection of characters for the password: " + passChar);
-return passChar;
+function preferences() {
+do{
+  inputU = prompt("Use Upper Case: Y N").toLowerCase();
+  inputL = prompt("Use Lower Case: Y N").toLowerCase();
+  inputN = prompt("Use Numeric: Y N").toLowerCase();
+  inputS = prompt("User Special Char: Y N").toLowerCase();
+
+  if(inputU !== "y" && inputU !== "n" ||
+  inputL !== "y" && inputL !== "n" ||
+  inputN !== "y" && inputN !== "n" ||
+  inputS !== "y" && inputS !== "n"){
+    alert("something was blank start over");
+  } 
+
+
+}
+  //using the input in some logic to check and use what was selected
+   //proofing the answer
+  while(inputU !== "y" && inputU !== "n" ||
+      inputL !== "y" && inputL !== "n" ||
+      inputN !== "y" && inputN !== "n" ||
+      inputS !== "y" && inputS !== "n") 
+
+  if (inputU === "y") {
+    console.log(inputU);
+    uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    console.log(uCase);
+  }
+  else if (inputU === "n") {
+    uCase = "";
+  }
+  if (inputL === "y") {
+    lCase = "abcdefghijklmnopqrstuvwxyz";
+    console.log(lCase);
+  } else if (inputL === "n") {
+    lCase = "";
+  }
+  if (inputN === "y") {
+    numStr = "1234567890";
+    console.log(numStr);
+  } else if (inputN === "n") {
+    numStr = "";
+  }
+  if (inputS === "y") {
+    specChar = "!@#$%^&*()";
+    console.log(specChar);
+  } else if (inputS === "n") {
+    specChar = "";
+  }
+
+ 
+
+
+  //create on string out of the selecting options 
+  let passChar = uCase.concat(lCase, numStr, specChar);
+  //checking what we are working with
+  console.log("this is the new selection of characters for the password: " + passChar);
+  return passChar;
 }
 // Write password to the #password input
-function generatePassword(){
-const passChar = preferences();
+function generatePassword() {
+  const passChar =  preferences();
 
-console.log("set gneratePasswod is working with: " + passChar);
-//do while to repeat the prompt if the user does not enter an int between 8 and 128
-do{
-  pLength = prompt("Password needs to be between 8 and 128 char long: ");
-  if(pLength < 8){
-    alert("password length too small");
+  console.log("set gneratePasswod is working with: " + passChar);
+  //do while to repeat the prompt if the user does not enter an int between 8 and 128
+  do {
+    pLength = prompt("Password needs to be between 8 and 128 char long: ");
+    if (pLength < 8) {
+      alert("password length too small");
+    }
+    if (pLength > 128) {
+      alert("password length too big")
+    }
+  } while (pLength < 8 || pLength > 128)//end while
+
+  let pWord = ""; //pWord starting empty 
+
+  for (let i = 0; i < pLength; i++) {
+    pWord = pWord + passChar.charAt(Math.floor(Math.random() * passChar.length - 1));
   }
-  if(pLength > 128){
-    alert("password length too big")
-  }
-}while(pLength < 8||pLength > 128)//end while
-
-let pWord=""; //pWord starting empty 
-
-for(let i = 0; i<pLength;i++){
-  pWord = pWord + passChar.charAt(Math.floor(Math.random() * passChar.length-1));
-}
   return pWord;
 }
 
@@ -88,3 +120,5 @@ function writePassword() {
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+//<button id='test' class ="btn" > test this </button>
+document.getElementById('test').addEventListener('click', testDo);
